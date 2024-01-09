3ba82bd00b29ed7d20e0c253772317dd117607f1
sardor <Sardorshavkatillayev@gmail.com>
2024-01-09 14:27:51 +0500

    initial commit

---
+++ src/main/java/com/example/jenkins/CalculatorController.java
@@ -0,0 +1,20 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/add")
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        return a + b;
+    }
+
+//    @GetMapping("/subtract")
+//    public Integer subtract(@RequestParam Integer a, @RequestParam Integer b) {
+//        return a - b;
+//    }
+
+}
---
+++ src/test/java/com/example/jenkins/CalculatorControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.jenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+@SpringBootTest
+class CalculatorControllerTest {
+
+    @Test
+    void add() throws IOException, InterruptedException {
+        Integer a = 9;
+        Integer b = 1;
+
+        HttpClient client = HttpClient.newBuilder().build();
+
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create("http://localhost:8084/add?a=%d&b=%d"
+                        .formatted(a, b))).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+        int i = Integer.parseInt(body);
+        Assertions.assertEquals(a + b, i);
+
+
+    }
+
+//    @Test
+//    void subtract() throws IOException, InterruptedException {
+//        Integer a = 10;
+//        Integer b = 9;
+//
+//        HttpClient client = HttpClient.newBuilder().build();
+//        HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8084/subtract?a=%d&b=%d"
+//                        .formatted(a, b))).
+//                build();
+//        HttpResponse<String> response =
+//                client.send(request, HttpResponse.BodyHandlers.ofString());
+//        String body = response.body();
+//        int i = Integer.parseInt(body);
+//        Assertions.assertEquals(a - b, i);
+//    }
+}

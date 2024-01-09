92fa4069975b0ef6459de122533478552b4cfcf1
sardor <Sardorshavkatillayev@gmail.com>
2024-01-09 15:59:46 +0500

    jenkins pol v2

--- src/main/java/com/example/jenkins/Just.java
+++ src/main/java/com/example/jenkins/Just.java
@@ -4,4 +4,8 @@ public class Just {
     public static void main(String[] args) {
         System.out.println("sardor");
     }
+    public static int birontaMetodu(int sayi1, int sayi2) {
+        int toplam = sayi1 + sayi2;
+        return toplam;
+    }
 }
